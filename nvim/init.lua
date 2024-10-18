88a1b3efe2aef68901a2ebf4008eccd6a442e5de
jonathan-guillot_emcor <jonathan_guillot@emcor.net>
2024-10-18 20:59:18 +0000

    Command a different way

--- nvim/init.lua
+++ nvim/init.lua
@@ -104,4 +104,4 @@ cmd.packadd('cfilter') -- Allows filtering the quickfix list with :cfdo
 vim.g.sqlite_clib_path = require('luv').os_getenv('LIBSQLITE')
 
 -- Theming
-  cmd.[[colorscheme nord]]
+vim.cmd([[colorscheme nord]])
