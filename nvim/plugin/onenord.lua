5831cf8a39c917d02bca35719b37363fdab3b42c
jdguillot <jdguillot@outlook.com>
2024-10-19 08:00:27 -0700

    Fixed Onenode theming

--- nvim/plugin/onenord.lua
+++ nvim/plugin/onenord.lua
@@ -2,13 +2,13 @@
 require('onenord').setup({
   disable = {
     background = true,
-    float_background = true,
+    -- float_background = true,
   },
   custom_colors = {
     -- gray = "#9A72EB",
     -- light_gray = "#9A72EB",
     -- fg_light = "#9A72EB",
     -- fg = "#9A72EB",
-    -- bg = "NONE",
+    bg = "#9A72EB80",
   },
 })
