7a874297d2d6f9032011a963873df9a66290abe0
jdguillot <jdguillot@outlook.com>
2024-10-21 07:11:05 -0700

    configuring highlights in nvim

--- nvim/plugin/onenord.lua
+++ nvim/plugin/onenord.lua
@@ -1,14 +1,26 @@
 -- Lua
+
+local colors = require("onenord.colors").load()
+
 require('onenord').setup({
+
+  -- fade_nc = true,
+
   disable = {
     background = true,
-    -- float_background = true,
+    float_background = true,
+  },
+  custom_highlights = {
+    TelescopeSelection = { bg = colors.bg },
+    TelescopeSelectionCaret = { bg = colors.green },
+    Pmenu = { bg = colors.bg },
+    ColorColumn = { bg = '#9A72EB' },
   },
   custom_colors = {
     -- gray = "#9A72EB",
     -- light_gray = "#9A72EB",
     -- fg_light = "#9A72EB",
-    -- fg = "#9A72EB",
-    bg = "#9A72EB80",
+    -- fg = "#D57780",
+    -- bg = "#9A72EB80",
   },
 })
